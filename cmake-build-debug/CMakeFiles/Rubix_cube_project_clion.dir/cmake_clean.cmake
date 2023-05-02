a66bfdc1be3c9b285a969545a2e726346d0010f2
nobody15 <pranavphatale2@gmail.com>
2023-05-02 15:37:17 +0530

    initial files

---
+++ cmake-build-debug/CMakeFiles/Rubix_cube_project_clion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rubix_cube_project_clion.dir/main.cpp.obj"
+  "CMakeFiles/Rubix_cube_project_clion.dir/main.cpp.obj.d"
+  "Rubix_cube_project_clion.exe"
+  "Rubix_cube_project_clion.exe.manifest"
+  "Rubix_cube_project_clion.pdb"
+  "libRubix_cube_project_clion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rubix_cube_project_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Rubix_cube_project_clion.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Rubix_cube_project_clion.
+# This may be replaced when dependencies are built.
